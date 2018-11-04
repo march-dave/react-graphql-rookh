8f7bb9f682a72985710f386a9e205a66d8123ffd
march-dave <ysangy@gmail.com>
2018-11-04 07:07:21 -0500

    add graphql, apollo server

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// Graphql Apollo Server 
+const { ApolloServer, gql } = require('apollo-server-express');
+
+
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
+
+app.use(cors(), bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
-app.listen(port);
+app.listen(port, ()=> {
+  console.log(`Server running on the: ${port}`)
+});
