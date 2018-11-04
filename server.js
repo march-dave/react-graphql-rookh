11b8119ec98cc98a9c3cb8f6c73d195e4986e7de
march-dave <ysangy@gmail.com>
2018-11-04 07:14:18 -0500

    add graphql Query, Mutation, Resolvers

--- server.js
+++ server.js
@@ -16,11 +16,61 @@ app.use(express.static(__dirname));
 app.use(cors(), bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+
+const todos = [
+  {id: 1, task: 'rook room', completed: false},
+  {id: 2, task: 'queen room', completed: true},
+  {id: 3, task: 'king room', completed: true},
+  {id: 4, task: 'bishop room', completed: true}
+]
+
+const typeDefs = gql`
+  type Todo {
+    id: Int
+    task: String
+    completed: Boolean
+  }
+
+  type Query {
+    getTodos: [Todo]
+    getTodo(id: Int): Todo
+  }
+
+  type Mutation {
+    addTodo(task: String, completed: Boolean): Todo
+  }
+`;
+
+const getById = id => {
+  console.log(id);
+  const fitleredTodo = todos.filter( c => c.id === id ? c : null )
+  return fitleredTodo[0];
+}
+
+const resolvers = {
+  Query: {
+    getTodos: () => todos,
+    getTodo: (_, {id}) => getById(id)
+  },
+  Mutation: {
+    addTodo: (_, args) => {
+      const todo = { task: args.task, completed: args.completed};
+      todos.push(todo);
+      return todo;
+    } 
+  }
+};
+
+const server = new ApolloServer({typeDefs, resolvers, introspection: true, playground: true});
+server.applyMiddleware({app});
+
+
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
-app.listen(port, ()=> {
-  console.log(`Server running on the: ${port}`)
+app.listen(port, () => {
+  console.log(`Express listening on port: ${server.graphqlPath} ${port}`);
 });
