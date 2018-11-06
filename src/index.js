9d8f4457da52974501d92d7d76f7491058a0e115
march-dave <ysangy@gmail.com>
2018-11-05 20:11:20 -0500

    graphql uri mapping

--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { ApolloProvider } from "react-apollo";
 
-ReactDOM.render(<App />, document.getElementById("index"));
+const myClient = new ApolloClient({
+  url: "https://react-graphql-rookh.herokuapp.com/graphql"
+});
+
+ReactDOM.render(
+  <ApolloProvider client={myClient}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("index")
+);
