692bb34b667a85171b9ef5ccc4f3e0f709b0d591
march-dave <ysangy@gmail.com>
2018-11-05 20:00:04 -0500

    add apollo client uri, gql, Query

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
+import { ApolloClient } from "Apollo-boost";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+const getTodos = gql`
+
+    getTodos {
+        id
+        task 
+        completed
+    }
+
+`;
+
+const myClient = new ApolloClient({
+  url: "https://react-graphql-rookh.herokuapp.com/graphql"
+});
 
 class App extends Component {
   render() {
-    return <div>Hello React! You are ready for heroku</div>;
+    return <Query>Hello React! You are ready for heroku</Query>;
   }
 }
 
