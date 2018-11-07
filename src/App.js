5acf1524e864829844e1ab26375c4135d24ce0b4
march-dave <ysangy@gmail.com>
2018-11-06 20:10:33 -0500

    apollo client use parameter

--- src/App.js
+++ src/App.js
@@ -12,16 +12,33 @@ const startQ = gql`
   }
 `;
 
+const startQ2 = gql`
+  {
+    getTodo(id: 2) {
+      id
+      task
+      completed
+    }
+  }
+`;
+
 class App extends Component {
   render() {
     return (
-      <Query query={startQ}>
-        {({ loading, error, data }) => {
+      // <Query query={startQ}>
+      //   {({ loading, error, data }) => {
+      //     if (loading) return "loading";
+      //     if (error) return "error";
+      //     if (data) {
+      //       return data.getTodos.map(c => c.task);
+      //     }
+      //   }}
+      // </Query>
+      <Query query={startQ2} variables={{ id: 2 }}>
+        {({ loading, data, error }) => {
           if (loading) return "loading";
           if (error) return "error";
-          if (data) {
-            return data.getTodos.map(c => c.task);
-          }
+          if (data) return data.getTodo.task;
         }}
       </Query>
     );
