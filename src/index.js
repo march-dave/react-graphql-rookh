e52a539400395207bf7ad6e6847eb13d05d6edcf
march-dave <ysangy@gmail.com>
2018-11-06 19:42:59 -0500

    code rollback

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { ApolloProvider } from "react-apollo";
-import ApolloClient from "apollo-boost";
 
-const myClient = new ApolloClient({
-  uri: "https://react-graphql-rookh.herokuapp.com/graphql"
-});
-
-ReactDOM.render(
-  <ApolloProvider client={myClient}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById("index")
-);
+ReactDOM.render(<App />, document.getElementById("index"));
