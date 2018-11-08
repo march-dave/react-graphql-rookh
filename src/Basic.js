97560c73ece4425e0b9ee1eaf1602d79a054bdba
march-dave <ysangy@gmail.com>
2018-11-07 19:35:59 -0500

    redux store create

--- src/Basic.js
+++ src/Basic.js
@@ -4,7 +4,7 @@ class Basic extends Component {
     render() {
         return (
             <div>
-                
+                <input type="text"/>
             </div>
         );
     }
--- src/BasicInfo.js
+++ src/BasicInfo.js
@@ -4,7 +4,7 @@ class BasicInfo extends Component {
     render() {
         return (
             <div>
-                
+                <input type="text"/>
             </div>
         );
     }
