ca9df6ef7b9a53ff00b952ce7d50a72162c07916
march-dave <ysangy@gmail.com>
2018-11-08 20:12:12 -0500

    implement apollo clinet in BasicInfo

--- src/BasicInfo.js
+++ src/BasicInfo.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+const startQ = gql`
+  {
+    getTodos {
+      id
+      task
+      completed
+    }
+  }
+`;
 
 class BasicInfo extends Component {
-    render() {
-        return (
-            <div>
-                <input type="text"/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <Query query={startQ}>
+        {({ loading, data, error }) => {
+          if (loading) return "loading";
+          if (error) return "error";
+          if (data) return data.getTodos.map(c => c.task);
+        }}
+      </Query>
+    );
+  }
 }
 
 export default BasicInfo;
