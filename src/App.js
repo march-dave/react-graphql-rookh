04075980dde19b696a730093349198365124a825
march-dave <ysangy@gmail.com>
2018-11-08 19:44:57 -0500

    code clean up

--- src/App.js
+++ src/App.js
@@ -1,46 +1,14 @@
 import React, { Component } from "react";
-import { Query } from "react-apollo";
-import gql from "graphql-tag";
-
-const startQ = gql`
-  {
-    getTodos {
-      id
-      task
-      completed
-    }
-  }
-`;
-
-const startQ2 = gql`
-  {
-    getTodo(id: 2) {
-      id
-      task
-      completed
-    }
-  }
-`;
+import Basic from "./Basic";
+import BasicInfo from "./BasicInfo";
 
 class App extends Component {
   render() {
     return (
-      // <Query query={startQ}>
-      //   {({ loading, error, data }) => {
-      //     if (loading) return "loading";
-      //     if (error) return "error";
-      //     if (data) {
-      //       return data.getTodos.map(c => c.task);
-      //     }
-      //   }}
-      // </Query>
-      <Query query={startQ2} variables={{ id: 2 }}>
-        {({ loading, data, error }) => {
-          if (loading) return "loading";
-          if (error) return "error";
-          if (data) return data.getTodo.task;
-        }}
-      </Query>
+      <div>
+        <Basic />
+        <BasicInfo />
+      </div>
     );
   }
 }
