cb526ebb8e15b120451c0679f5055baea5d36d47
march-dave <ysangy@gmail.com>
2018-11-09 22:40:19 -0500

    code style replace to ES6

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
 
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-// Graphql Apollo Server 
-const { ApolloServer, gql } = require('apollo-server-express');
+// Graphql Apollo Server
+const { ApolloServer, gql } = require("apollo-server-express");
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
@@ -16,11 +16,11 @@ app.use(cors(), bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const todos = [
-  {id: 1, task: 'rook room', completed: false},
-  {id: 2, task: 'queen room', completed: true},
-  {id: 3, task: 'king room', completed: true},
-  {id: 4, task: 'bishop room', completed: true}
-]
+  { id: 1, task: "rook room", completed: false },
+  { id: 2, task: "queen room", completed: true },
+  { id: 3, task: "king room", completed: true },
+  { id: 4, task: "bishop room", completed: true }
+];
 
 const typeDefs = gql`
   type Todo {
@@ -41,91 +41,98 @@ const typeDefs = gql`
 
 const getById = id => {
   console.log(id);
-  const fitleredTodo = todos.filter( c => c.id === id ? c : null )
+  const fitleredTodo = todos.filter(c => (c.id === id ? c : null));
   return fitleredTodo[0];
-}
+};
 
 const resolvers = {
   Query: {
     getTodos: () => todos,
-    getTodo: (_, {id}) => getById(id)
+    getTodo: (_, { id }) => getById(id)
   },
   Mutation: {
     addTodo: (_, args) => {
-      const todo = { task: args.task, completed: args.completed};
+      const todo = { task: args.task, completed: args.completed };
       todos.push(todo);
       return todo;
-    } 
+    }
   }
 };
 
-const server = new ApolloServer({typeDefs, resolvers, introspection: true, playground: true});
-server.applyMiddleware({app});
-
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true
+});
+server.applyMiddleware({ app });
 
 const authData = {
-  email: 'dave@abc.com',
-  password: '1234',
-  nickname: 'march-dave'
+  email: "dave@abc.com",
+  password: "1234",
+  nickname: "march-dave"
 };
 
-const passport = require('passport'),
-  LocalStrategy = require('passport-local').Strategy;
+const passport = require("passport"),
+  LocalStrategy = require("passport-local").Strategy;
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser(function(user, done) {
-  console.log('serializeUser', user);
+passport.serializeUser((user, done) => {
+  console.log("serializeUser", user);
   done(null, user.email);
 });
 
-passport.deserializeUser(function(id, done) {
-  console.log('deserializeUser', id);
+passport.deserializeUser((id, done) => {
+  console.log("deserializeUser", id);
   done(null, authData);
 });
 
-passport.use(new LocalStrategy(
-  {
-    usernameField: 'email',
-    passwordField: 'pwd'
-  },
-  function (username, password, done) {
-    console.log('LocalStrategy', username, password);
-    if(username === authData.email){
-      console.log(1);
-      if(password === authData.password){
-        console.log(2);
-        return done(null, authData, {
-          message: 'Welcome.'
-        });
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "pwd"
+    },
+    (username, password, done) => {
+      console.log("LocalStrategy", username, password);
+      if (username === authData.email) {
+        console.log(1);
+        if (password === authData.password) {
+          console.log(2);
+          return done(null, authData, {
+            message: "Welcome."
+          });
+        } else {
+          console.log(3);
+          return done(null, false, {
+            message: "Incorrect password."
+          });
+        }
       } else {
-        console.log(3);
+        console.log(4);
         return done(null, false, {
-          message: 'Incorrect password.'
+          message: "Incorrect username."
         });
       }
-    } else {
-      console.log(4);
-      return done(null, false, {
-        message: 'Incorrect username.'
-      });
     }
-  }
-));
+  )
+);
 
-app.post('/auth/login_process',
-  passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/auth/login',
+app.post(
+  "/auth/login_process",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/auth/login"
     // failureFlash:true,
     // successFlash:true
-  }));
-
+  })
+);
 
 // send the user to index html page inspite of the url
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
 app.listen(port, () => {
