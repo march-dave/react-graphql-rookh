32fcafd21f8cdb57b03b55b7aa9d38ea50eaaf8b
march-dave <ysangy@gmail.com>
2018-11-09 22:25:57 -0500

    add passport

--- server.js
+++ server.js
@@ -63,6 +63,66 @@ const server = new ApolloServer({typeDefs, resolvers, introspection: true, playg
 server.applyMiddleware({app});
 
 
+const authData = {
+  email: 'egoing777@gmail.com',
+  password: '111111',
+  nickname: 'egoing'
+};
+
+const passport = require('passport'),
+  LocalStrategy = require('passport-local').Strategy;
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  console.log('serializeUser', user);
+  done(null, user.email);
+});
+
+passport.deserializeUser(function(id, done) {
+  console.log('deserializeUser', id);
+  done(null, authData);
+});
+
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'email',
+    passwordField: 'pwd'
+  },
+  function (username, password, done) {
+    console.log('LocalStrategy', username, password);
+    if(username === authData.email){
+      console.log(1);
+      if(password === authData.password){
+        console.log(2);
+        return done(null, authData, {
+          message: 'Welcome.'
+        });
+      } else {
+        console.log(3);
+        return done(null, false, {
+          message: 'Incorrect password.'
+        });
+      }
+    } else {
+      console.log(4);
+      return done(null, false, {
+        message: 'Incorrect username.'
+      });
+    }
+  }
+));
+
+app.post('/auth/login_process',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/auth/login',
+    failureFlash:true,
+    successFlash:true
+  }));
+
+
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
