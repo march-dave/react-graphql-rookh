c37f4ce2e08a0c9aeb30d671bbfa8853c41a9bcb
march-dave <ysangy@gmail.com>
2018-11-11 16:11:13 -0500

    up to date

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
 
 class Login extends Component {
-  handleSubmit = () => {};
+  state = {
+    email: "",
+    password: ""
+  };
+
+  handleSubmit = () => {
+    alert("submit");
+    event.preventDefault();
+  };
 
   render() {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <input type="text" placeholder="email" />
-          <input type="text" placeholder="password" />
+          <input
+            type="text"
+            placeholder="email"
+            onChange={this.onHanldeChange}
+          />
+          <input
+            type="text"
+            placeholder="password"
+            onChange={this.onHanldeChange}
+          />
           <input type="submit" />
         </form>
       </div>
