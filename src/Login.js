83edd5957ecf7501c8b82decac2523a777309c68
march-dave <ysangy@gmail.com>
2018-11-11 15:59:53 -0500

    add from

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 
 class Login extends Component {
+  handleSubmit = () => {};
+
   render() {
     return (
       <div>
-        <input type="text" placeholder="email" />
-        <input type="text" placeholder="password" />
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="email" />
+          <input type="text" placeholder="password" />
+          <input type="submit" />
+        </form>
       </div>
     );
   }
