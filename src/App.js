c630f7ce2676b70a6e7bc5ae1ebf3d9acef06e6c
march-dave <ysangy@gmail.com>
2018-11-10 21:37:49 -0500

    add login ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Basic from "./Basic";
 import BasicInfo from "./BasicInfo";
-
+import Login from "./Login";
 class App extends Component {
   render() {
     return (
---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <input type="text" placeholder="email" />     <input type="text" placeholder="password"/>
+            </div>
+        );
+    }
+}
+
+export default Login;
