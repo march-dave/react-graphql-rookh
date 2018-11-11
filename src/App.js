bcb2acf62c9270e5f8ff218e6dc02969dcbe9a21
march-dave <ysangy@gmail.com>
2018-11-10 21:38:40 -0500

    code arrange

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
       <div>
         <Basic />
         <BasicInfo />
+        <Login />
       </div>
     );
   }
--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Login extends Component {
-    render() {
-        return (
-            <div>
-                <input type="text" placeholder="email" />     <input type="text" placeholder="password"/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <input type="text" placeholder="email" />
+        <input type="text" placeholder="password" />
+      </div>
+    );
+  }
 }
 
 export default Login;
