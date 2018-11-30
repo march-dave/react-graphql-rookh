09f5b2b5f9f5e5895f54e1a8a0e0ede8dfaea5a7
march-dave <ysangy@gmail.com>
2018-11-29 20:20:21 -0500

    redux error

--- src/userReducer.js
+++ src/userReducer.js
@@ -1,21 +1,21 @@
-import reducer from "./Reducers";
-import { combineReducers } from "redux";
+// import reducer from "./Reducers";
+// import { combineReducers } from "redux";
 
-// const initialState = {
-//     username: "Jane"
-// }
+// // const initialState = {
+// //     username: "Jane"
+// // }
 
-const userReducer = (state = {}, action) => {
-  switch (action.type) {
-    case "SET_USER":
-      return {
-        Object.assign({}, state , {username: action.user} )
-      };
-    default:
-      return state;
-  }
-};
+// const userReducer = (state = {}, action) => {
+//   switch (action.type) {
+//     case "SET_USER":
+//       return {
+//         Object.assign({}, state , {username: action.user} )
+//       };
+//     default:
+//       return state;
+//   }
+// };
 
-const reducers = combineReducers(reducer, userReducer);
+// const reducers = combineReducers(reducer, userReducer);
 
-export default reducers;
+// export default reducers;
